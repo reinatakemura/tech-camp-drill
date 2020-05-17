39d19e4aecabb9a3232de78a42e1ac764fe19f13
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-05-17 21:57:03 +0900

    update drill77.rb

--- drill77.rb
+++ drill77.rb
@@ -1,6 +1,9 @@
 # my解答
 def count_evens(ary)
-  puts (ary.select { |num| num % 2 == 0}).length
+  # puts (ary.select { |num| num % 2 == 0}).length
+  # puts ary.count{ |num| num.even?}
+  # puts ary.select(&:even?).count
+  puts ary.select(&:even?).length  # &はメソッドにこれはブロック引数ですと伝える効果がある, :で始まるリテラルはSymbol
 end
 
 ary = [3, 4, 6, 9, 0]
@@ -8,15 +11,15 @@ count_evens(ary)
 
 #####################################
 # 解答
-def count_evens(nums)
-  count = 0
-  nums.each do |num|
-    if num.even?
-      count += 1
-    end     
-  end
-  puts count
-end
+# def count_evens(nums)
+#   count = 0
+#   nums.each do |num|
+#     if num.even?
+#       count += 1
+#     end     
+#   end
+#   puts count
+# end
 
-nums =[3, 4, 6, 9, 0]
-count_evens(nums)
+# nums =[3, 4, 6, 9, 0]
+# count_evens(nums)
