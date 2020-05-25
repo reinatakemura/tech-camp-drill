ed3b39ef669c7dfa5693ba95c4c6f3eed4851a22
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-05-25 22:50:35 +0900

    Create drill4.rb

---
+++ drill4.rb
@@ -0,0 +1,33 @@
+下記の（１）を変換して（２）にするためのコードを記述してください。
+（１） "Today" " is" " sunny"
+（２） "Today is sunny"
+
+"Today" + " is" + " sunny"
+
+
+下記の（１）を変換して（２）にするためのコードを記述してください。
+（１） "Ruby, Rails, JavaScript"
+（２） ["Ruby", " Rails", " JavaScript"]
+
+文字列を分割する
+"Ruby, Rails, JavaScript".split(",")
+
+下記の（１）を変換して（２）にするためのコードを記述してください。
+（１） "rails"
+（２） "Rails"
+
+"rails".capitalize
+
+
+下記の（１）を変換して（２）にするためのコードを記述してください。
+（１） "abcde"
+（２） "edcba"
+
+"abcde".reverse
+
+
+下記の（１）を変換して（２）にするためのコードを記述してください。
+（１） "Hello, world"
+（２） "HELLO, WORLD"
+
+"Hello, world".upcase
