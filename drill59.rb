114e653c2a260355655ba7f3f84c5973c1f7f2ce
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-06-21 18:55:14 +0900

    Create drill59.rb

---
+++ drill59.rb
@@ -0,0 +1,25 @@
+# あなたは警官です。aとｂ二人の容疑者の取り調べをしています。
+# どちらも証言がTrue、またはFalseであればその証言はTrueです。
+# しかしどちらかがFalseでTrueであればその証言はFalse、と出力するメソッドを作りましょう。
+
+# 呼び出し方：
+# police_trouble(a, b)
+
+# 出力例：
+# police_trouble(true, false) → False
+# police_trouble(false, false) → True
+# police_trouble(true, true) → True
+
+def police_trouble(a, b)
+  if a && b || !a && !b
+    puts "True"
+  else
+    puts "False"
+  end
+end
+
+a = false
+b = true
+police_trouble(a, b)
+
+# 参考：https://www.javadrive.jp/ruby/if/index4.html
