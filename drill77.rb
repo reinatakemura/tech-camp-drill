8d8ce0611265eaebc9eba2f81c527ec96d3a06fa
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-07-04 21:15:42 +0900

    Update drill77.rb

--- drill77.rb
+++ drill77.rb
@@ -1,25 +1,14 @@
-# my解答
+# 配列要素にある数値から偶数を数えて出力するメソッドを作りましょう。
+
+# 出力例：
+# count_evens([2, 1, 2, 3, 4]) → 3
+# count_evens([2, 2, 0]) → 3
+# count_evens([1, 3, 5]) → 0
+
 def count_evens(ary)
-  # puts (ary.select { |num| num % 2 == 0}).length
-  # puts ary.count{ |num| num.even?}
-  # puts ary.select(&:even?).count
-  puts ary.select(&:even?).length  # &はメソッドにこれはブロック引数ですと伝える効果がある, :で始まるリテラルはSymbol
+  puts ary.count(& :even?)
 end
 
-ary = [3, 4, 6, 9, 0]
-count_evens(ary)
-
-#####################################
-# 解答
-# def count_evens(nums)
-#   count = 0
-#   nums.each do |num|
-#     if num.even?
-#       count += 1
-#     end     
-#   end
-#   puts count
-# end
-
-# nums =[3, 4, 6, 9, 0]
-# count_evens(nums)
+count_evens([2, 1, 2, 3, 4])
+count_evens([2, 2, 0])
+count_evens([1, 3, 5])
